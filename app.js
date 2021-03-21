cf08ebb8d2fae1295ca35032826565c4f4f4c40c
Blazej Szafranski <Szafranski@Blazejs-MacBook-Pro.local>
2021-03-21 16:44:03 +0100

    Heroku ready

--- app.js
+++ app.js
@@ -139,6 +139,6 @@ app.get('/posts/:postTitle', (req, res) => {
   });
 });
 
-app.listen(3000, function() {
+app.listen(process.env.PORT || 3002, function() {
   console.log("Server started on port 3000");
 });
